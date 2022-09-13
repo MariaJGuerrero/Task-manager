baa354c299fef08981042fa68ecda770717e40cc
Tork <guerreromjosefa@gmail.com>
2022-09-13 19:16:38 +0200

    creamos formularios

--- src/components/containers/task-list.jsx
+++ src/components/containers/task-list.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { LEVELS } from "../models/levels.enum";
 import { Task } from "../models/task.class";
 import TaskComponent from "../pure/task";
 
 const TaskListComponent = () => {
+
+    const [tasks, setTasks] = useState(defaultTask);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() =>{
+        console.log('Task state has been modified');
+        setLoading(false);
+        return () => {
+            console.log('TaskList component is going to unmount')
+        }
+    }, [tasks]);
+
     const defaultTask = new Task('example', 'default description', false, LEVELS.NORMAL)
+
+    const changeCompleted = (id) => {
+        console.log('TO DO: cambiar el estado de una tarea')
+    }
 
     return(
         <div>
